d3fe391d750eb3f940f381129b9dbf05e1858cf5
tzipporah <zippy@funtoo.org>
2022-10-10 19:34:25 -0400

    Exam 1 retry!

--- src/Exam1/Exam1.java
+++ src/Exam1/Exam1.java
@@ -3,6 +3,9 @@ package Exam1;
 import java.util.Scanner;
 
 public class Exam1 {
+    // Tzipporah Robbins
+    //Exam 1 Make-up
+    // October 10, 2022
 
     public static int validateN(Scanner s){
         boolean valid = false;
@@ -24,31 +27,28 @@ public class Exam1 {
             }
         }
         return n;
-    }
-    
-    public static int betweenTwoAndSeven(int n){
+    } // validation
+
+    public static int betweenTwoAndSeven(int n) {
+
+        int eachInt = 0;
         int count = 0;
-        for (int i = 0; i < n.length; i++){
-            if (i > 2 && i < 7){
+        while (n > 0){
+            eachInt = eachInt + (n % 10);
+            if (eachInt < 7 && eachInt > 2){
                 count++;
             }
-            }
-        return count;
+            eachInt = 0;
+            n = n / 10;
         }
-
-        //I only know how to index a number because it is usually
-    // an input straight from the user.
-    // Here, I didn't know what to do because the value is an integer after
-    // it gets validated.
-    // I can't use the length method, so I think I'm going to give up.
-    // Hope I can get partial credit.
-    
-
+        return count;
+    } // between two and seven
     public static void main(String [] args){
+        System.out.println("Exam 1");
         Scanner s = new Scanner(System.in);
-
         int n = validateN(s);
-        System.out.println(betweenTwoAndSeven(n));
-    }
+        System.out.printf("The number of digits between" +
+                " two and seven in your number is %d.",betweenTwoAndSeven(n));
+    } // main
 
 }
